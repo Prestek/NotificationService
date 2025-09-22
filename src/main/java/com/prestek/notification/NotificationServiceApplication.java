1515b5ce78c1b87db12170d1af1adf3103530bb3
mateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-09-21 20:56:22 -0500

    feature: intial commit strucutre

---
+++ src/main/java/com/prestek/notification/NotificationServiceApplication.java
@@ -0,0 +1,33 @@
+package com.prestek.notification;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(NotificationServiceApplication.class);
+
+	public static void main(String[] args) {
+		// Load environment variables from .env file only if not already set
+		try {
+			Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+			dotenv.entries().forEach(entry -> {
+				String key = entry.getKey();
+				
+				// Only set the system property if it is not already set
+				logger.info("Setting system property: {}={}", key, entry.getValue());
+				System.setProperty(key, entry.getValue());
+			});
+		} catch (Exception e) {
+			// Continue without .env file
+		}
+		SpringApplication.run(NotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prestek/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestek.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
